5402694f683f2452007ab1f4021ffb59c1bbd71b
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2023-10-05 13:03:47 +0500

    Add user name validation function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,6 +1,6 @@
 function factorial(n) {
   if (n < 0) {
-    return "undefined";
+    return 1;
   } else if (n === 0 || n === 1) {
     return 1;
   } else {
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,11 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (typeof name === 'string' && name !== null) {
+    let userName = name.trim();
+
+    if (userName.length >= 4 && /^[a-zA-Z0-9а-яА-Я]+$/.test(userName) && userName.indexOf(' ') === -1) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt('Dmitriy');
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
