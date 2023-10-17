563ff14aa94a21c4a3bf3b817a7fd6496ff52fa6
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2023-10-17 22:26:03 +0500

    Filtering by Range

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let filterAttrs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      filterAttrs.push(arr[i]);
+    }
+  }
+
+  return filterAttrs;
 }
