6b294a3e818987c7cc8c01b318893fa6b5da8b94
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2023-10-18 16:41:20 +0500

    Changing text to uppercase

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,11 +1,5 @@
 function filterRange(arr, a, b) {
-  let filterAttrs = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= a && arr[i] <= b) {
-      filterAttrs.push(arr[i]);
-    }
-  }
-
-  return filterAttrs;
+  return arr.filter(function(item) {
+    return item >= a && item <= b;
+  });
 }
