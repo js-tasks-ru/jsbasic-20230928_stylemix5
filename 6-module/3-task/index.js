43043891ecfec75e8c63d37b61622407df943596
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2023-11-09 21:56:26 +0500

    Finished 6-module all task

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,94 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentSlide = 0;
+
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+          ${this.slides.map(slide => this.renderSlide(slide)).join('')}
+        </div>
+      </div>
+    `);
+
+    this.updateArrowsVisibility();
+  }
+
+  renderSlide(slide) {
+    return `
+      <div class="carousel__slide" data-id="${slide.id}">
+        <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  addEventListeners() {
+    this.elem.addEventListener('click', event => {
+      const arrow = event.target.closest('.carousel__arrow');
+      if (arrow) {
+        arrow.classList.contains('carousel__arrow_right') ? this.nextSlide() : this.prevSlide();
+      }
+
+      const button = event.target.closest('.carousel__button');
+      if (button) {
+        const slideId = button.closest('.carousel__slide').dataset.id;
+        this.triggerProductAddEvent(slideId);
+      }
+    });
+  }
+
+  nextSlide() {
+    if (this.currentSlide < this.slides.length - 1) {
+      this.currentSlide++;
+      this.updateSlide();
+    }
+  }
+
+  prevSlide() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+      this.updateSlide();
+    }
+  }
+
+  updateSlide() {
+    const inner = this.elem.querySelector('.carousel__inner');
+    const slideWidth = this.elem.offsetWidth;
+
+    inner.style.transform = `translateX(-${this.currentSlide * slideWidth}px)`;
+    this.updateArrowsVisibility();
+  }
+
+  updateArrowsVisibility() {
+    const arrows = this.elem.querySelectorAll('.carousel__arrow');
+    arrows[0].style.display = this.currentSlide === this.slides.length - 1 ? 'none' : '';
+    arrows[1].style.display = this.currentSlide === 0 ? 'none' : '';
+  }
+
+  triggerProductAddEvent(slideId) {
+    const productAddEvent = new CustomEvent('product-add', {
+      detail: slideId,
+      bubbles: true
+    });
+
+    this.elem.dispatchEvent(productAddEvent);
   }
 }
